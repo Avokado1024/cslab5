3cbd432efe1e7ca7d131f38dd70a539b423c8376
Avokado1024 <144896565+Avokado1024@users.noreply.github.com>
2024-10-31 10:23:32 +0300

    Update 5_1.cs

--- 5_1.cs
+++ 5_1.cs
@@ -1,5 +1,4 @@
 using System;
-
 class MyMatrix
 {
     private int[,] matrix;
@@ -8,8 +7,6 @@ class MyMatrix
     private int minValue;
     private int maxValue;
     private Random random;
-
-    // Конструктор, принимающий число строк и столбцов, и диапазон случайных чисел
     public MyMatrix(int rows, int cols, int minValue, int maxValue)
     {
         this.rows = rows;
@@ -17,12 +14,9 @@ class MyMatrix
         this.minValue = minValue;
         this.maxValue = maxValue;
         this.random = new Random();
-
         matrix = new int[rows, cols];
         Fill();
     }
-
-    // Метод для заполнения матрицы случайными числами
     public void Fill()
     {
         for (int i = 0; i < rows; i++)
@@ -33,13 +27,9 @@ class MyMatrix
             }
         }
     }
-
-    // Метод для изменения размера матрицы с копированием значений
     public void ChangeSize(int newRows, int newCols)
     {
         int[,] newMatrix = new int[newRows, newCols];
-
-        // Копирование значений из старой матрицы в новую
         for (int i = 0; i < Math.Min(rows, newRows); i++)
         {
             for (int j = 0; j < Math.Min(cols, newCols); j++)
@@ -47,8 +37,6 @@ class MyMatrix
                 newMatrix[i, j] = matrix[i, j];
             }
         }
-
-        // Заполнение новых элементов случайными числами
         for (int i = 0; i < newRows; i++)
         {
             for (int j = 0; j < newCols; j++)
@@ -59,13 +47,10 @@ class MyMatrix
                 }
             }
         }
-
         matrix = newMatrix;
         rows = newRows;
         cols = newCols;
     }
-
-    // Метод для вывода части матрицы на консоль
     public void ShowPartialy(int startRow, int endRow, int startCol, int endCol)
     {
         for (int i = startRow; i <= endRow; i++)
@@ -77,8 +62,6 @@ class MyMatrix
             Console.WriteLine();
         }
     }
-
-    // Метод для вывода всей матрицы на консоль
     public void Show()
     {
         for (int i = 0; i < rows; i++)
@@ -90,8 +73,6 @@ class MyMatrix
             Console.WriteLine();
         }
     }
-
-    // Индексатор для доступа к элементам матрицы
     public int this[int index1, int index2]
     {
         get
@@ -118,38 +99,28 @@ class MyMatrix
         }
     }
 }
-
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Введите минимальное значение для случайных чисел:");
+        Console.WriteLine("Введите минимальное значение для генерации случайных чисел:");
         int minValue = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("Введите максимальное значение для случайных чисел:");
+        Console.WriteLine("Введите максимальное значение для генерации случайных чисел:");
         int maxValue = int.Parse(Console.ReadLine());
-
         Console.WriteLine("Введите количество строк матрицы:");
         int rows = int.Parse(Console.ReadLine());
-
         Console.WriteLine("Введите количество столбцов матрицы:");
         int cols = int.Parse(Console.ReadLine());
-
         MyMatrix matrix = new MyMatrix(rows, cols, minValue, maxValue);
-
         Console.WriteLine("Исходная матрица:");
         matrix.Show();
-
         Console.WriteLine("Изменение размера матрицы:");
-        matrix.ChangeSize(5, 5);
+        matrix.ChangeSize(6, 6);
         matrix.Show();
-
         Console.WriteLine("Частичный вывод матрицы:");
         matrix.ShowPartialy(1, 3, 1, 3);
-
-
         Console.WriteLine("Изменение элемента матрицы через индексатор:");
-        matrix[0, 0] = 99;
+        matrix[0, 0] = 100;
         matrix.Show();
     }
 }
